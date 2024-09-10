1b1ed8b0fd4f474ec1728a90e5515c6b23799634
Multitasking <surajgupta@MultiTasking.local>
2024-09-10 17:43:47 +0530

    commission

--- Terminal/terminal.py
+++ Terminal/terminal.py
@@ -4,18 +4,21 @@ pygame.init()
 clock = pygame.time.Clock() 
 screen = pygame.display.set_mode([600, 600]) 
 base_font = pygame.font.Font("Terminal/Consolas.ttf", 12) 
-user_text = ''
-user_text += """New OS Terminal Session
-""" 
-user_text += ">> "
+user_text = '>> '
 user_text_dict = dict()
-input_rect = pygame.Rect(0, 0, 600, 600)
-color_passive = pygame.Color('black') 
-color = color_passive 
+color_passive = pygame.Color('black')
+color = color_passive
   
 active = False
   
 while True: 
+    starting_rect = pygame.Rect(0, 0, 600, 10)
+    input_rect = (pygame.Rect(0,15,600,600))
+    screen.fill((0, 0, 0)) 
+    pygame.draw.rect(screen, color, starting_rect) 
+    text_surface = base_font.render("New OS terminal session", True, (255, 255, 255)) 
+    screen.blit(text_surface, (starting_rect.x+5, starting_rect.y+5)) 
+    input_rect.w = max(100, text_surface.get_width()+10) 
     for event in pygame.event.get(): 
         if event.type == pygame.QUIT: 
             pygame.quit() 
@@ -25,12 +28,15 @@ while True:
                 user_text = user_text[:-1] 
             else: 
                 user_text += event.unicode
-            if event.key == pygame.K_KP_ENTER:
-                user_text += "\n"
-    screen.fill((0, 0, 0)) 
+            if event.key == pygame.K_ENTER:
+                print("wtf")
+                pygame.draw.rect(screen, color, pygame.Rect(0,30,600,600)) 
+                text_surface = base_font.render("Incorrect command", True, (255, 255, 255)) 
+                screen.blit(text_surface, (input_rect.x+5, input_rect.y+5)) 
+
     pygame.draw.rect(screen, color, input_rect) 
-    text_surface = base_font.render(user_text, True, (255, 255, 255)) 
-    screen.blit(text_surface, (input_rect.x+5, input_rect.y+5)) 
+    textinput = base_font.render(user_text, True, (255, 255, 255)) 
+    screen.blit(textinput, (input_rect.x+5, input_rect.y+5)) 
     input_rect.w = max(100, text_surface.get_width()+10) 
     pygame.display.flip() 
     clock.tick(60) 
